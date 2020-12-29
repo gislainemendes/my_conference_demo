ec557a3660d24ac522aff6e48da8d6a2cf8228cc
Gislaine Mendes <gislainemrocha@gmail.com>
2020-12-29 16:25:54 -0300

    comentando classe PersistenceConfiguration

--- src/main/java/com/example/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/example/demo/config/PersistenceConfiguration.java
@@ -8,12 +8,12 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
+/*    @Bean
     public DataSource dataSource(){
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:mysql://localhost:3306/conference_demo?useTimezone=true&serverTimezone=America/Sao_Paulo");
         builder.username("root").password("root");
         System.out.println("My custom datasource bean has been initialized");
         return builder.build();
-    }
+    }*/
 }
